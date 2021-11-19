e9d47916f141f61efbc58fd1168c4a84c1053b28
ranstar74 <34216771+ranstar74@users.noreply.github.com>
2021-11-20 01:02:28 +0300

    Don't update components until handle assigned and add some methods in Pool

--- RageComponent/Core/ComponentCollection.cs
+++ RageComponent/Core/ComponentCollection.cs
@@ -7,6 +7,12 @@ namespace RageComponent.Core
     /// <summary>
     /// Defines a collection of <see cref="Component"/>'s.
     /// </summary>
+    /// <remarks>
+    /// Must be used in pair with <see cref="IComponentObject"/> and <see cref="ComponentObjectPool"/>.
+    /// <para>
+    ///  Otherwise <see cref="OnUpdate"/> won't be called.
+    ///  </para>
+    /// </remarks>
     public abstract class ComponentCollection
     {
         /// <summary>
@@ -17,6 +23,15 @@ namespace RageComponent.Core
         /// Made as workaround of Unity's GameObject.
         /// </remarks>  
         public object GameObject => gameObject;
+
+        /// <summary>
+        /// Gets or sets a value indicating if <see cref="OnUpdate"/> will be called or not.
+        /// </summary>
+        /// <remarks>
+        /// Used to disable component collection before object was added to pool
+        /// because some components may access handle.
+        /// </remarks>
+        internal bool IsEnabled { get; set; } = false;
 
         private readonly List<Component> components = new List<Component>();
         private readonly object gameObject;
@@ -111,6 +126,9 @@ namespace RageComponent.Core
         /// </summary>
         public void OnUpdate()
         {
+            if (!IsEnabled)
+                return;
+
             for(int i = 0; i < components.Count; i++)
             {
                 var component = components[i];
--- RageComponent/Core/ComponentObjectPool.cs
+++ RageComponent/Core/ComponentObjectPool.cs
@@ -1,15 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RageComponent.Core
 {
     /// <summary>
     /// This class manages a collection of <see cref="IComponentObject"/>.
     /// </summary>
-    public class ComponentObjectPool : IEnumerable<IComponentObject>
+    public class ComponentObjectPool : IList<IComponentObject>
     {
-        private static int handleCounter = int.MinValue;
+        private static int handleCounter = 1;
         private readonly Dictionary<long, IComponentObject> componentObjects = new Dictionary<long, IComponentObject>();
+
+        /// <summary>
+        /// Gets number of elements in the <see cref="ComponentObjectPool"/>.
+        /// </summary>
+        public int Count => componentObjects.Count;
+
+        /// <summary>
+        /// Gets whether the <see cref="ComponentObjectPool"/> is read only or not.
+        /// </summary>
+        /// <remarks>
+        /// Hardcoded to false.
+        /// </remarks>
+        public bool IsReadOnly => false;
 
         /// <summary>
         /// Creates a new instance of <see cref="ComponentObjectPool"/>.
@@ -21,6 +35,16 @@ namespace RageComponent.Core
         }
 
         /// <summary>
+        /// Gets element at specific index.
+        /// </summary>
+        /// <remarks>
+        /// Set does nothing.
+        /// </remarks>
+        /// <param name="index">Index of the element.</param>
+        /// <returns>Element at the specified index.</returns>
+        public IComponentObject this[int index] { get => componentObjects[index]; set { } }
+
+        /// <summary>
         /// Adds a <see cref="IComponentObject"/> to the collection.
         /// </summary>
         /// <param name="componentObject"><see cref="IComponentObject"/> to add.</param>
@@ -28,7 +52,12 @@ namespace RageComponent.Core
         {
             componentObjects.Add(handleCounter, componentObject);
 
-            componentObject.SetComponentHandle(handleCounter++);
+            componentObject.SetComponentHandle(handleCounter);
+
+            // Enable components after setting handle
+            componentObject.GetComponents().IsEnabled = true;
+
+            handleCounter++;
         }
 
         /// <summary>
@@ -43,12 +72,29 @@ namespace RageComponent.Core
         }
 
         /// <summary>
-        /// Removes a <see cref="IComponentObject"/> from the collection by its handle.
+        /// Removes a <see cref="IComponentObject"/> from the collection by its handle and invalidates its handle.
         /// </summary>
         /// <param name="componentObjectHandle">Handle of <see cref="IComponentObject"/> to remove.</param>
         public void Remove(int componentObjectHandle)
         {
+            IComponentObject componentObject = componentObjects[componentObjectHandle];
+            componentObject.InvalidateHandle();
+
             componentObjects.Remove(componentObjectHandle);
+        }
+
+        /// <summary>
+        /// Removes a <see cref="IComponentObject"/> from the collection and invalidates its handle.
+        /// </summary>
+        /// <param name="componentObject"><see cref="IComponentObject"/> to remove.</param>
+        public void Remove(IComponentObject componentObject)
+        {
+            if (componentObject == null)
+                return;
+
+            componentObject.InvalidateHandle();
+
+            Remove(componentObject.ComponentHandle);
         }
 
         /// <summary>
@@ -63,16 +109,37 @@ namespace RageComponent.Core
         }
 
         /// <summary>
-        /// Calls <see cref="ComponentCollection.OnDispose"/> for every component of the <see cref="IComponentObject"/>.
+        /// Calls Dispose for every <see cref="IComponentObject"/>.
+        /// <para>
+        /// Invalidates handle.
+        /// </para>
         /// </summary>
         public void Dispose()
         {
             foreach (IComponentObject componentObject in componentObjects.Values)
             {
-                componentObject.GetComponents().OnDispose();
+                componentObject.InvalidateHandle();
+                componentObject.Dispose();
             }
+            componentObjects.Clear();
         }
 
+        /// <summary>
+        /// In constract from <see cref="Dispose"/>, disposes only components.
+        /// </summary>
+        public void DisposeComponents()
+        {
+            foreach (IComponentObject componentObject in componentObjects.Values)
+            {
+                componentObject.InvalidateHandle();
+                componentObject.GetComponents().OnDispose();
+            }
+            componentObjects.Clear();
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public IEnumerator<IComponentObject> GetEnumerator()
         {
             return componentObjects.Values.GetEnumerator();
@@ -81,6 +148,81 @@ namespace RageComponent.Core
         IEnumerator IEnumerable.GetEnumerator()
         {
             return componentObjects.Values.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Determines index of item in the <see cref="ComponentObjectPool"/>.
+        /// </summary>
+        /// <param name="item">Item to check.</param>
+        /// <returns>Index of item.</returns>
+        public int IndexOf(IComponentObject item)
+        {
+            return componentObjects.Values
+                .ToList()
+                .IndexOf(item);
+        }
+
+        /// <summary>
+        /// Removes item at index.
+        /// </summary>
+        /// <param name="index">Index of item needs to be removed.</param>
+        public void RemoveAt(int index)
+        {
+            IComponentObject item = componentObjects[index];
+            item.InvalidateHandle();
+            componentObjects.Remove(index);
+        }
+
+        /// <summary>
+        /// Determines whether the <see cref="ComponentObjectPool"/> contains item.
+        /// </summary>
+        /// <param name="item">Item to check.</param>
+        /// <returns>True if <see cref="ComponentObjectPool"/> contains item, otherwise False.</returns>
+        public bool Contains(IComponentObject item)
+        {
+            return componentObjects.Values
+                .ToList()
+                .Contains(item);
+        }
+
+        /// <summary>
+        /// Not supported.
+        /// </summary>
+        /// <exception cref="System.NotSupportedException"></exception>
+        public void Insert(int index, IComponentObject item)
+        {
+            throw new System.NotSupportedException();
+        }
+
+        /// <summary>
+        /// Not supported.
+        /// </summary>
+        /// <exception cref="System.NotSupportedException"></exception>
+        public void Clear()
+        {
+            foreach (IComponentObject componentObject in componentObjects.Values)
+            {
+                componentObject.InvalidateHandle();
+            }
+            componentObjects.Clear();
+        }
+
+        /// <summary>
+        /// Not supported.
+        /// </summary>
+        /// <exception cref="System.NotSupportedException"></exception>
+        public void CopyTo(IComponentObject[] array, int arrayIndex)
+        {
+            throw new System.NotSupportedException();
+        }
+
+        /// <summary>
+        /// Not supported.
+        /// </summary>
+        /// <exception cref="System.NotSupportedException"></exception>
+        bool ICollection<IComponentObject>.Remove(IComponentObject item)
+        {
+            throw new System.NotSupportedException();
         }
     }
 }
