67c56a1679d2326afe2e6eb12065da70966afa8b
ranstar74 <34216771+ranstar74@users.noreply.github.com>
2021-12-28 20:22:22 +0300

    Add Yield for component and small refactor

--- RageComponent/Core/Component.cs
+++ RageComponent/Core/Component.cs
@@ -4,35 +4,28 @@ using System;
 
 namespace RageComponent
 {
-    /// <summary>
-    /// Defines a component that does some specific functionality.
-    /// </summary>
-    public abstract class Component : IDisposable
+    /// <summary>Defines a component that does some specific functionality.</summary>
+    /// <remarks><see cref="Start"/>, <see cref="Update"/>, <see cref="Dispose"/> and <see cref="Reload"/> 
+    /// could be overriden, but can't be called.
+    /// <para>It was made because without <see cref="ComponentCollection"/> some component functionality
+    /// won't be working properly.</para></remarks>
+    public abstract class Component
     {
-        /// <summary>
-        /// Component collection the <see cref="Component"/> created in.
-        /// </summary>
+        /// <summary>Component collection the <see cref="Component"/> created in.</summary>
         public ComponentCollection Components => components;
 
-        /// <summary>
-        /// Shortcut for to game player.
-        /// </summary>
+        /// <summary>Shortcut for to game player.</summary>
         public Ped GPlayer => Game.Player.Character;
 
-        /// <summary>
-        /// If Set To False, <see cref="Update"/> will be skipped.
-        /// </summary>
+        /// <summary>If Set To False, <see cref="Update"/> will be skipped.</summary>
         public bool IsEnabled { get; set; } = true;
 
-        /// <summary>
-        /// Defines how often update is called.
-        /// </summary>
-        /// <remarks>
-        /// -1 is every tick, 250 is every 250ms
-        /// </remarks>
+        /// <summary>Defines a time interval of calling <see cref="Update"/> in ms.</summary>
+        /// <remarks>Set -1 to call update every tick.</remarks>
         public int UpdateTime { get; set; } = -1;
 
         internal int NextUpdateTime = 0;
+        internal int YieldTicks = 0;
         private readonly ComponentCollection components;
 
         /// <summary>
@@ -44,48 +37,48 @@ namespace RageComponent
             this.components = components;
         }
 
-        /// <summary>
-        /// Gets component parent.
-        /// </summary>
+        /// <summary>Skips execution of <see cref="Update"/> for given amount of frames.</summary>
+        /// <param name="ticks">A natural number of frames to skip.</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void Yield(int ticks = 1)
+        {
+            if(ticks <= 0)
+                throw new ArgumentOutOfRangeException(
+                    paramName: nameof(ticks), 
+                    message: "Amount of ticks to yield must be a natural number.");
+
+            YieldTicks = ticks;
+        }
+
+        /// <summary>Gets component parent.</summary>
         /// <returns>A parent of the <see cref="IComponentObject"/></returns>
-        public T GetParent<T>() where T : class
+        protected T GetParent<T>() where T : class
         {
             return (T)components.GameObject;
         }
 
-        /// <summary>
-        /// Called when component begins to exist.
-        /// </summary>
+        /// <summary>Called when component begins to exist.</summary>
         public virtual void Start()
         {
 
         }
 
-        /// <summary>
-        /// Called every frame.
-        /// </summary>
+        /// <summary>Called every frame.</summary>
         public virtual void Update()
         {
             
         }
 
-        /// <summary>
-        /// Called when <see cref="IComponentObject"/> being disposed.
-        /// <para>
-        /// Dispose component related objects here, for static use <see cref="Reload"/>.
-        /// </para>
+        /// <summary>Called when <see cref="IComponentObject"/> being disposed.
+        /// <para>Dispose component related objects here, for static use <see cref="Reload"/>.</para>
         /// </summary>
         public virtual void Dispose()
         {
 
         }
 
-        /// <summary>
-        /// Called on script reload.
-        /// <para>
-        /// Useful for disposing static objects.
-        /// </para>
-        /// </summary>
+        /// <summary>Called on script reload.
+        /// <para>Useful for disposing static objects.</para></summary>
         public virtual void Reload()
         {
 
